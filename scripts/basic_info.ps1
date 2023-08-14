4904807433a2d5d783a52406f0029803f4113c13
Smukx ♠ <96696929+Whitecat18@users.noreply.github.com>
2023-08-14 23:46:48 +0530

    Create basic_info.ps1

---
+++ scripts/basic_info.ps1
@@ -0,0 +1,37 @@
+<#
+Scripts managed by https://github.com/Whitecat18
+For More info . visit https://github.com/Whitecat18/Powershell-Scripts-for-Hackers-and-Pentesters
+#>
+
+function Get-PCInformation {
+    $pcInfo = @{
+        'ComputerName'   = $env:COMPUTERNAME
+        'Manufacturer'   = (Get-WmiObject Win32_ComputerSystem).Manufacturer
+        'Model'          = (Get-WmiObject Win32_ComputerSystem).Model
+        'RAM'            = '{0:N2} GB' -f ((Get-WmiObject Win32_ComputerSystem).TotalPhysicalMemory / 1GB)
+        'Processor'      = (Get-WmiObject Win32_Processor).Name
+        'Storage'        = (Get-WmiObject Win32_DiskDrive | Measure-Object Size -Sum).Sum / 1TB -as [int]
+        'WindowsVersion' = (Get-WmiObject Win32_OperatingSystem).Caption
+    }
+    
+    return $pcInfo
+}
+
+$pcInformation = Get-PCInformation
+
+$output = @"
+Computer Information:
+Computer Name: $($pcInformation['ComputerName'])
+Manufacturer: $($pcInformation['Manufacturer'])
+Model: $($pcInformation['Model'])
+RAM: $($pcInformation['RAM'])
+Processor: $($pcInformation['Processor'])
+Storage: $($pcInformation['Storage']) TB
+Windows Version: $($pcInformation['WindowsVersion'])
+"@
+
+$outputPath = Join-Path $env:TEMP "PC_Information.txt"
+
+$output | Out-File -FilePath $outputPath
+
+Write-Host "PC information has been saved to $outputPath"
